bc7ed425a1a99a65afcea7c7155245acd5350a9d
divin3circle <omutelema.silas21@students.dkut.ac.ke>
2024-10-04 01:16:40 +0300

    Add Drawer component and integrate with Navbar; refactor Hero component and utility functions

--- src/components/app/hero.tsx
+++ src/components/app/hero.tsx
@@ -7,18 +7,18 @@ function Hero() {
   return (
     <div className="h-screen relative">
       <div className="flex flex-col justify-center items-center">
-        <p className="text-2xl font-semibold text-title font-body mt-52">
+        <p className="text-2xl text-title font-body mt-52">
           Guide,
           <span className="text-body text-green-500">Uplift,</span>
           Impact
         </p>
-        <h1 className="text-[5rem] font-semibold text-title font-body">
+        <h1 className="md:text-[5rem] text-4xl font-semibold text-title font-body text-center">
           Computer Society
         </h1>
-        <h1 className="text-[5rem] -mt-8 mb-12 font-semibold text-title font-body">
+        <h1 className="md:text-[5rem] text-4xl md:-mt-8 md:mb-12 mb-8 font-semibold text-title font-body text-center">
           of Kimathi
         </h1>
-        <p className="text-2xl max-w-2xl text-body font-body text-center mt-12">
+        <p className="md:text-2xl text-lg px-4 max-w-2xl text-body font-body text-center mt-12">
           A student-run community within Dedan Kimathi ​University of Technology
           dedicated to cultivating ​a strong tech community and empowerment!
         </p>
@@ -26,22 +26,22 @@ function Hero() {
       <img
         src={students}
         alt="students"
-        className="h-80 w-64 r rounded-full object-cover absolute right-0 top-0 rotate-[10deg]"
+        className="h-80 w-64 r rounded-full object-cover absolute right-0 top-0 rotate-[10deg] hidden"
       />
       <img
         src={students2}
         alt="students"
-        className="h-72 w-96 rounded-[70%] object-cover absolute left-0 top-0 rotate-[4deg]"
+        className="h-72 w-96 rounded-[70%] object-cover absolute left-0 top-0 rotate-[4deg] hidden"
       />
       <img
         src={students}
         alt="students"
-        className="h-[20rem] w-[15rem] r rounded-[85%] object-cover absolute right-0 bottom-36 -rotate-[10deg]"
+        className="h-[20rem] w-[15rem] r rounded-[85%] object-cover abmd:solute right-0 bottom-36 -rotate-[10deg] hidden"
       />
       <img
         src={students}
         alt="students"
-        className="h-64 w-[22rem] r rounded-[90%] object-cover absolute left-0 bottom-36 rotate-[19deg]"
+        className="h-64 w-[22rem] r rounded-[90%] object-cover md:absolute left-0 bottom-36 rotate-[19deg] hidden"
       />
     </div>
   );
--- src/components/app/navbar.tsx
+++ src/components/app/navbar.tsx
@@ -1,5 +1,17 @@
 import React from "react";
 import logo from "../../../public/csoklogo.png";
+import { FaBurger } from "react-icons/fa6";
+import { IoMdMenu } from "react-icons/io";
+import {
+  Drawer,
+  DrawerClose,
+  DrawerContent,
+  DrawerDescription,
+  DrawerFooter,
+  DrawerHeader,
+  DrawerTitle,
+  DrawerTrigger,
+} from "../ui/drawer";
 
 function Navbar() {
   return (
@@ -7,19 +19,50 @@ function Navbar() {
       <div>
         <img src={logo} alt="logo" className="w-28 h-28" />
       </div>
-      <div className="flex items-center justify-between gap-4">
-        <h1 className="font-body mx-2 text-lg cursor-pointer">Home</h1>
-        <h1 className="font-body mx-2 text-lg cursor-pointer">About Us</h1>
-        <h1 className="font-body mx-2 text-lg cursor-pointer">Objectives</h1>
-        <h1 className="font-body mx-2 text-lg cursor-pointer">Leads</h1>
-        <h1 className="font-body mx-2 text-lg cursor-pointer">Events</h1>
-        <h1 className="font-body mx-2 text-lg cursor-pointer">Gallery</h1>
+      <div className="md:flex items-center justify-between gap-2 lg:gap-4 hidden">
+        <h1 className="font-body mx-2 lg:text-lg cursor-pointer">Home</h1>
+        <h1 className="font-body mx-2 lg:text-lg cursor-pointer">About Us</h1>
+        <h1 className="font-body mx-2 lg:text-lg cursor-pointer">Objectives</h1>
+        <h1 className="font-body mx-2 lg:text-lg cursor-pointer">Leads</h1>
+        <h1 className="font-body mx-2 lg:text-lg cursor-pointer">Events</h1>
+        <h1 className="font-body mx-2 lg:text-lg cursor-pointer">Gallery</h1>
       </div>
-      <div>
-        <button className="bg-primary px-12 py-2 rounded-xl text-lg font-body text-white">
+      <div className="hidden md:flex items-center justify-center mr-8">
+        <button className="bg-primary lg:px-12 px-4 py-2 rounded-xl lg:text-lg font-body text-white mr-8">
           Join us
         </button>
       </div>
+
+      <Drawer>
+        <DrawerTrigger className="md:hidden flex items-center justify-center mx-8">
+          <IoMdMenu className="text-3xl text-body" color="#17252a" size={48} />
+        </DrawerTrigger>
+        <DrawerContent>
+          <DrawerHeader>
+            <DrawerTitle>
+              <div className="flex items-center justify-between">
+                <img src={logo} alt="logo" className="w-28 h-28" />
+                <button className="bg-primary px-8 mx-4 py-2 rounded-xl text-lg font-body text-white">
+                  Join us
+                </button>
+              </div>
+            </DrawerTitle>
+          </DrawerHeader>
+          <div className="flex gap-8 flex-col">
+            <h1 className="font-body mx-2 text-lg cursor-pointer">Home</h1>
+            <h1 className="font-body mx-2 text-lg cursor-pointer">About Us</h1>
+            <h1 className="font-body mx-2 text-lg cursor-pointer">
+              Objectives
+            </h1>
+            <h1 className="font-body mx-2 text-lg cursor-pointer">Leads</h1>
+            <h1 className="font-body mx-2 text-lg cursor-pointer">Events</h1>
+            <h1 className="font-body mx-2 text-lg cursor-pointer">Gallery</h1>
+          </div>
+          <DrawerClose className="mb-20">
+            <button>Close</button>
+          </DrawerClose>
+        </DrawerContent>
+      </Drawer>
     </div>
   );
 }
