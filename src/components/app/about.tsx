4a7d6f9c18a84d5926065a021fbacb12a8551ee2
divin3circle <omutelema.silas21@students.dkut.ac.ke>
2024-10-04 02:13:05 +0300

    Update About component button styles for improved visibility and aesthetics

--- src/components/app/about.tsx
+++ src/components/app/about.tsx
@@ -6,7 +6,7 @@ function About() {
     <div className="my-4 mx-2">
       <div className="flex flex-col md:flex-row w-full items-center justify-between">
         <div className="flex flex-col gap-4 md:w-[60%] w-full">
-          <h1 className="font-body text-title text-4xl text-center md:text-start md:mb-4 md:text-5xl">
+          <h1 className="font-body text-title text-4xl lg:text-[4rem] text-center md:text-start md:mb-4 md:text-5xl">
             Who We Are
           </h1>
           <p className="px-2 mt-2 md:text-lg">
@@ -26,7 +26,7 @@ function About() {
         </div>
       </div>
       <div className="flex items-center justify-center md:justify-normal">
-        <button className="bg-transparent border-2 border-black px-12 mx-2 text-black font-body text-lg py-2 rounded-2xl mt-4">
+        <button className="bg-transparent border-4 border-black px-12 mx-2 text-black font-body text-lg py-2 rounded-3xl mt-4 lg:text-xl">
           Join Now
         </button>
       </div>
