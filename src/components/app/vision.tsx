a33a8bcaad6a284f6defc5b942e85e5374228afd
divin3circle <omutelema.silas21@students.dkut.ac.ke>
2024-10-08 23:25:59 +0300

    Refactor icon imports for improved icon support

--- src/components/app/vision.tsx
+++ src/components/app/vision.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import {
   IconTrendingUp,
-  IconBugFilled,
+  IconBulbFilled,
   IconBriefcase2Filled,
 } from "@tabler/icons-react";
 function Vision() {
@@ -11,11 +11,31 @@ function Vision() {
         Our Vision & Mission
       </h1>
       <div className="h-1 rounded-lg bg-gray-500 w-[15%]"></div>
-      <div className="flex flex-col md:flex-row md:justify-between justify-center items-center">
-        <div className="flex items-center justify-between flex-col">
-          <IconTrendingUp size={56} className="text-gray-500" />
-          <div className="bg-[#004C4A] rounded-md px-2 py-4">
-            <p className="text-white font-body text-lg">
+      <div className="flex flex-col md:flex-row md:justify-between justify-center items-center md:gap-4 gap-8 mt-16 mx-4">
+        <div className="flex rounded-lg items-center justify-between flex-col gap-8">
+          <IconTrendingUp size={64} className="text-gray-500" />
+          <div className="bg-[#004C4A] rounded-2xl py-12">
+            <p className="text-white font-body md:text-xl text-lg my-8 leading-relaxed gap-2 px-8">
+              To promote development of knowledge into technical skills among
+              students by hosting practical workshops, guest lectures and
+              meetups.
+            </p>
+          </div>
+        </div>
+        <div className="flex items-center justify-between flex-col gap-8">
+          <IconBulbFilled size={64} className="text-gray-500" />
+          <div className="bg-[#004C4A] rounded-2xl py-12">
+            <p className="text-white font-body md:text-xl text-lg my-8 leading-relaxed gap-2 px-8">
+              To promote development of knowledge into technical skills among
+              students by hosting practical workshops, guest lectures and
+              meetups.
+            </p>
+          </div>
+        </div>
+        <div className="flex items-center justify-between flex-col gap-8">
+          <IconBriefcase2Filled size={64} className="text-gray-500" />
+          <div className="bg-[#004C4A] rounded-2xl py-12">
+            <p className="text-white font-body text-lg md:text-xl my-8 leading-relaxed gap-2 px-8">
               To promote development of knowledge into technical skills among
               students by hosting practical workshops, guest lectures and
               meetups.
